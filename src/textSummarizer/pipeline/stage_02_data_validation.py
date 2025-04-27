c987bc35039f462e9da82fae7c341d9968cf5ee4
PRINCY SONI <soniprincy02@gmail.com>
2025-04-27 11:39:43 +0530

    file

--- src/textSummarizer/pipeline/stage_02_data_validation.py
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -7,8 +7,10 @@ class DataValidationTrainingPipeline:
     def __init__(self):
         pass
 
+
     def main(self):
         config = ConfigurationManager()
         data_validation_config = config.get_data_validation_config()
         data_validation = DataValiadtion(config=data_validation_config)
         data_validation.validate_all_files_exist()
+        
